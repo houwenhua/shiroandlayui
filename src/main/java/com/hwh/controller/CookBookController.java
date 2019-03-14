1cec98aafad4484e99b067bcd7efab5c44333813
hwh <1319427512@qq.com>
2019-03-14 23:15:57 +0800

    增加菜谱的Controller层

---
+++ src/main/java/com/hwh/controller/CookBookController.java
@@ -0,0 +1,17 @@
+package com.hwh.controller;
+
+/**
+ * 功能描述：
+ *
+ * @Author houwenhua
+ * @Date 2018/12/22 19:02
+ */
+public class CookBookController {
+    public static final String DEF_CHATSET = "UTF-8";
+    public static final int DEF_CONN_TIMEOUT = 30000;
+    public static final int DEF_READ_TIMEOUT = 30000;
+    public static String userAgent =  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
+
+    //配置您申请的KEY
+    public static final String APPKEY ="fe88f9c00bba36c6358b7ff97c7ce84d";
+}
